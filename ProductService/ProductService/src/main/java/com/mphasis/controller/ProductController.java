d73e6efae190e71d9834bf0eef33322b0a7b24fb
Muthyala Sushmitha <muthyala.sushmitha@mphasis.com>
2024-05-14 15:49:59 +0530

    reference

---
+++ ProductService/ProductService/src/main/java/com/mphasis/controller/ProductController.java
@@ -0,0 +1,62 @@
+package com.mphasis.controller;
+
+import java.util.UUID;
+
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mphasis.command.CreateProductCommand;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private CommandGateway commandGateway;
+	
+	@PostMapping
+	public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
+		
+		CreateProductCommand createProductCommand = CreateProductCommand.builder()
+		.price(createProductRestModel.getPrice())
+		.quantity(createProductRestModel.getQuantity())
+		.title(createProductRestModel.getTitle())
+		.productId(UUID.randomUUID().toString())
+		.build();
+		
+		String returnValue;
+		
+		try {
+			returnValue = commandGateway.sendAndWait(createProductCommand);
+		}catch (Exception ex) {
+			returnValue = ex.getLocalizedMessage();
+		}
+		return returnValue;
+	}
+
+	@GetMapping
+	public String getProduct() {
+		return " HTTP GET Handled: " + env.getProperty("local.server.port");
+	}
+	
+	@PutMapping
+	public String updateProduct() {
+		return " HTTP PUT Handled";
+	}
+	
+	@DeleteMapping
+	public String deleteProduct() {
+		return " HTTP DELETE Handled";
+	}
+}
